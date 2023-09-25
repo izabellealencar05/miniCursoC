6a5d806932364bc55080da9bfe078b37a19915c3
iza <126572391+izabellealencar05@users.noreply.github.com>
2023-09-25 11:54:01 -0300

    Create aula1 - q3.c

---
+++ aula1 - q3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+//Faça um programa que recebe um valor inteiro [ scanf("%d", &variavel) ], soma esse valor com 9, salva na própria variavel e o mostre na tela.
+
+int main(){
+    int x;
+    printf("digite um numero: ");
+    scanf("%d", &x);
+    printf("a soma com 9 e: %d", x +9);
+}

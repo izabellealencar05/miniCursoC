644506681552933f067afe64b713f2417cb827c1
iza <126572391+izabellealencar05@users.noreply.github.com>
2023-09-25 14:30:07 -0300

    Update praEntregar1.c

--- praEntregar1.c
+++ praEntregar1.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
 //Receba um valor em Real (R$) (float) e mostre a conversão para Dólar(5 R$) e Euro(5.2 R$).
 //Faça junto o teste de mesa da questão.
-int main(){
+
+
+int main() {
     float real;
     char escolha;
+
     printf("digite um valor: ");
     scanf("%f", &real);
-    printf("voce quer transformar para Dolar (D) ou Euro (E)?");
-    scanf("%f", &escolha);
-}   
+    printf("voce quer transformar para Dolar (D/d) ou Euro (E/e)? ");
+    scanf("%s", &escolha);
+    
+    if (escolha == 'D'|| escolha == 'd'){
+        printf("a conversao de real para dolar fica: %.2f\n", real * 5);
+    }else if(escolha == 'E' || escolha == 'e'){
+        printf("a conversao de real para euro fica: %.2f\n", real * 5.2);
+    }else{
+        printf("nao existe essa opcao");
+        }
+    return 0;
+}
+

491a5a84a7c89a30e3b1d741b33e050a881d6046
iza <126572391+izabellealencar05@users.noreply.github.com>
2023-09-25 12:06:30 -0300

    Create praEntregar1.c

---
+++ praEntregar1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+//Receba um valor em Real (R$) (float) e mostre a conversão para Dólar(5 R$) e Euro(5.2 R$).
+//Faça junto o teste de mesa da questão.
+int main(){
+    float real;
+    char escolha;
+    printf("digite um valor: ");
+    scanf("%f", &real);
+    printf("voce quer transformar para Dolar (D) ou Euro (E)?");
+    scanf("%f", &escolha);
+}   

4f2adeec458a8482c09fca6e2c5338248146e76b
iza <126572391+izabellealencar05@users.noreply.github.com>
2023-09-27 11:15:21 -0300

    Update pra entregar3.c

--- pra entregar3.c
+++ pra entregar3.c
@@ -1,3 +1,24 @@
+//Faça um algoritmo que leia dois números inteiros e mostre o
+//resultado da multiplicação entre eles, se ambos forem positivos;
+//ou a soma, se pelo menos um deles for negativo.
+
+#include <stdio.h>
+
+int main(){
+    int x;
+    int y;
+
+  printf("digite um numero: ");
+  scanf("%d %d", &x, &y);
+    int mult = x * y;
+    int soma = x + y;
+  if(x >= 0 && y >= 0){
+    printf("a multiplicacao entre eles e: %d", mult);
+  }else {
+    printf("a soma entre eles e: %d", soma);
+  }
+}
+
 
 
 
